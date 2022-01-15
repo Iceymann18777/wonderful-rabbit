9f152874eb18f0dced7d1cd7a1fe2f2ec91daaca
Stackbit Code Editor <projects@stackbit.com>
2022-01-15 17:06:39 +0000

    advanced.js: changed

--- src/layouts/advanced.js
+++ src/layouts/advanced.js
@@ -1,73 +1,33 @@
-import { Account, Connection, PublicKey } from '@solana/web3.js';
-import { Market } from '@project-serum/serum';
+import React from 'react';
+import _ from 'lodash';
 
-let connection = new Connection('https://testnet.solana.com');
-let marketAddress = new PublicKey('...');
-let market = await Market.load(connection, marketAddress);
+import components, { Layout } from '../components/index';
+import { getPageUrl } from '../utils';
 
-// Fetching orderbooks
-let bids = await market.loadBids(connection);
-let asks = await market.loadAsks(connection);
-// L2 orderbook data
-for (let [price, size] of bids.getL2(20)) {
-  console.log(price, size);
-}
-// Full orderbook data
-for (let order of asks) {
-  console.log(
-    order.orderId,
-    order.price,
-    order.size,
-    order.side, // 'buy' or 'sell'
-  );
-}
+export default class Advanced extends React.Component {
+    render() {
+        const data = _.get(this.props, 'data');
+        const config = _.get(data, 'config');
+        const posts = _.get(this.props, 'posts');
+        const page = _.get(this.props, 'page');
+        const sections = _.get(page, 'sections');
+        const pageUrl = getPageUrl(page);
 
-// Placing orders
-let owner = new Account('...');
-let payer = new PublicKey('...'); // spl-token account
-await market.placeOrder(connection, {
-  owner,
-  payer,
-  side: 'buy', // 'buy' or 'sell'
-  price: 123.45,
-  size: 17.0,
-  orderType: 'limit', // 'limit', 'ioc', 'postOnly'
-});
-
-// Retrieving open orders by owner
-let myOrders = await market.loadOrdersForOwner(connection, owner.publicKey);
-
-// Cancelling orders
-for (let order of myOrders) {
-  await market.cancelOrder(connection, owner, order);
-}
-
-// Retrieving fills
-for (let fill of await market.loadFills(connection)) {
-  console.log(
-    fill.orderId,
-    fill.price,
-    fill.size,
-    fill.side,
-  );
-}
-
-// Settle funds
-for (let openOrders of await market.findOpenOrdersAccountsForOwner(
-  connection,
-  owner.publicKey,
-)) {
-  if (openOrders.baseTokenFree > 0 || openOrders.quoteTokenFree > 0) {
-    // spl-token accounts to which to send the proceeds from trades
-    let baseTokenAccount = new PublicKey('...');
-    let quoteTokenAccount = new PublicKey('...');
-
-    await market.settleFunds(
-      connection,
-      owner,
-      openOrders,
-      baseTokenAccount,
-      quoteTokenAccount,
-    );
-  }
+        return (
+            <Layout page={page} config={config}>
+                {_.map(sections, (section, index) => {
+                    const sectionType = _.get(section, 'type');
+                    const component = _.upperFirst(_.camelCase(sectionType));
+                    if (!component) {
+                        throw new Error(`page section does not have the 'type' property, page: ${pageUrl}`);
+                    }
+                    const Component = components[component];
+                    if (!Component) {
+                        throw new Error(`no component matching the page section's type: ${sectionType}`);
+                    }
+                    return  <Component key={index} section={section} data={data} posts={posts} />;
+                })}
+            </Layout>
+        );
+    }
 }
