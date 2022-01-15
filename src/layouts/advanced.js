49ccc8ea5e20abbfc0d87b9470a3658d6b4dc0b5
Stackbit Code Editor <projects@stackbit.com>
2022-01-15 16:35:34 +0000

    advanced.js: changed

--- src/layouts/advanced.js
+++ src/layouts/advanced.js
@@ -12,7 +12,7 @@
 import React from 'react';
 
 export default function
-f
+finosBanner
 class Footer extends React.Component {
   docUrl(doc, language = '') {
     const baseUrl = this.props.config.baseUrl;
