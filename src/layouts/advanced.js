20741b7efe5bd37fc0661fdfdfe70fda068d9e38
Stackbit Code Editor <projects@stackbit.com>
2022-01-15 16:36:29 +0000

    advanced.js: changed

--- src/layouts/advanced.js
+++ src/layouts/advanced.js
@@ -1,33 +1,135 @@
-import React from 'react';
-import _ from 'lodash';
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
-import components, { Layout } from '../components/index';
-import { getPageUrl } from '../utils';
 
-export default class Advanced extends React.Component {
-    render() {
-        const data = _.get(this.props, 'data');
-        const config = _.get(data, 'config');
-        const posts = _.get(this.props, 'posts');
-        const page = _.get(this.props, 'page');
-        const sections = _.get(page, 'sections');
-        const pageUrl = getPageUrl(page);
 
-        return (
-            <Layout page={page} config={config}>
-                {_.map(sections, (section, index) => {
-                    const sectionType = _.get(section, 'type');
-                    const component = _.upperFirst(_.camelCase(sectionType));
-                    if (!component) {
-                        throw new Error(`page section does not have the 'type' property, page: ${pageUrl}`);
-                    }
-                    const Component = components[component];
-                    if (!Component) {
-                        throw new Error(`no component matching the page section's type: ${sectionType}`);
-                    }
-                    return  <Component key={index} section={section} data={data} posts={posts} />;
-                })}
-            </Layout>
-        );
-    }
+
+
+const React = require('react');
+
+class Footer extends React.Component {
+  docUrl(doc, language = '') {
+    const baseUrl = this.props.config.baseUrl;
+    const docsUrl = this.props.config.docsUrl;
+    const defaultVersionShown = this.props.config.defaultVersionShown;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const versionPart = `${defaultVersionShown ? `${defaultVersionShown}/` : ''}`;
+    const langPart = `${language ? `${language}/` : ''}`;
+    return `${baseUrl}${docsPart}${versionPart}${langPart}${doc}`;
+  }
+
+  pageUrl(doc, language) {
+    const baseUrl = this.props.config.baseUrl;
+    return baseUrl + (language ? `${language}/` : '') + doc;
+  }
+
+  render() {
+    return (
+      <footer className="nav-footer" id="footer">
+        <section className="sitemap">     
+          <a href={this.props.config.baseUrl} className="nav-home">
+            {this.props.config.footerIcon && (
+              <img
+                src={this.props.config.baseUrl + this.props.config.footerIcon}
+                alt={this.props.config.title}
+              />
+            )}
+          </a>
+          <div>
+            <h5>Docs</h5>
+            <a href={this.docUrl('fo-intro', this.props.language)}>
+              Getting Started
+            </a>
+            <a href={this.docUrl('why-fo', this.props.language)}>
+              Why FINOS Financial Objects
+            </a>
+            <a href={this.docUrl('fo-library', this.props.language)}>
+              Objects Reference library
+            </a>
+            <a href={this.pageUrl('getinvolved', this.props.language)}>
+              Get Involved
+            </a>
+          </div>
+          <div>
+            <h5>Community</h5>
+            <a
+              href="https://www.finos.org/"
+              target="_blank"
+              rel="noreferrer noopener">
+              FINOS
+            </a>
+            <a
+              href="https://finosfoundation.atlassian.net/wiki/spaces/FO/overview"
+              target="_blank"
+              rel="noreferrer noopener">
+              Financial Objects Wiki
+            </a>
+            <a
+              href="https://groups.google.com/a/finos.org/forum/#!forum/fo-financial-objects"
+              target="_blank"
+              rel="noreferrer noopener">
+              Mailing List
+            </a>            
+            {/* <a href={this.pageUrl('users.html', this.props.language)}>
+              User Showcase
+            </a>
+            <a
+              href="http://stackoverflow.com/questions/tagged/"
+              target="_blank"
+              rel="noreferrer noopener">
+              Stack Overflow
+            </a>
+            <a href="https://discordapp.com/">Project Chat</a>
+            <a
+              href="https://twitter.com/"
+              target="_blank"
+              rel="noreferrer noopener">
+              Twitter
+            </a> */}
+          </div>
+          <div>
+            <h5>More</h5>
+            <div className="social">
+              <a
+                className="github-button" // part of the https://buttons.github.io/buttons.js script in siteConfig.js
+                href={this.props.config.repoUrl}
+                data-count-href={`${this.props.config.repoUrl}/stargazers`}
+                data-show-count="true"
+                data-count-aria-label="# stargazers on GitHub"
+                aria-label="Star this project on GitHub">
+                {this.props.config.projectName}
+              </a>
+            </div>
+            {this.props.config.twitterUsername && (
+              <div className="social">
+                <a
+                  href={`https://twitter.com/${this.props.config.twitterUsername}`}
+                  className="twitter-follow-button">
+                  Follow @{this.props.config.twitterUsername}
+                </a>
+              </div>
+            )}
+          </div>
+        </section>
+        <section className="finos finosBanner">
+          <a href="https://www.finos.org">
+            <img id="finosicon" src={`img/finos_wordmark.svg`} height='75px' alt="FINOS" title="FINOS"/>
+            <h2 id="proud">Proud project of the Fintech Open Source Foundation</h2>
+          </a>
+
+        </section>
+        
+        <section className="copyright">{this.props.config.copyright}</section>
+
+      </footer>
+    );
+  }
 }
+
+
+
+module.exports = Footer;
