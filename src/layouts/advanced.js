042bc92ee99292d1138b65699ae6a435e4188fca
Stackbit Code Editor <projects@stackbit.com>
2022-01-15 16:38:42 +0000

    advanced.js: changed

--- src/layouts/advanced.js
+++ src/layouts/advanced.js
@@ -5,13 +5,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const React = require('react');
+import React from 'react';
 
-const CompLibrary = require('../../core/CompLibrary.js');
+import { MarkdownBlock as _MarkdownBlock, Container as _Container, GridBlock as _GridBlock } from '../../core/CompLibrary.js';
 
-const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
-const Container = CompLibrary.Container;
-const GridBlock = CompLibrary.GridBlock;
+const MarkdownBlock = _MarkdownBlock; /* Used to read markdown */
+const Container = _Container;
+const GridBlock = _GridBlock;
 
 class HomeSplash extends React.Component {
   render() {
@@ -184,4 +184,4 @@ class Index extends React.Component {
   }
 }
 
-module.exports = Index;
+export default Index;
